33f3cfcb348f95cabe6d7275f90ea52e26aa949c
larrygf02 <raulgf_02@hotmail.com>
2019-04-16 00:18:52 -0500

    Metodos http

--- server/server.js
+++ server/server.js
@@ -1,9 +1,37 @@
 const express = require('express')
 const app = express()
+const bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
 
 app.get('/', function(req, res) {
     res.json('Hola mundo')
 })
+
+app.get('/usuario', function(req, res) {
+    res.json('Get Usuario')
+})
+
+app.post('/usuario', function(req, res) {
+    let body = req.body;
+    res.json({
+        body
+    })
+})
+
+
+app.put('/usuario/:id', function(req, res) {
+    let id = req.params.id;
+    res.json({
+        id
+    })
+})
+
+app.delete('/usuario', function(req, res) {
+    res.json('Delete usuario')
+})
+
 
 app.listen(3000, () => {
     console.log('Escuchando el puerto 3000');
