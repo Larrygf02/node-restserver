be6416f554ab0397f2c877b2688ba48591e4e725
larrygf02 <raulgf_02@hotmail.com>
2019-04-18 16:28:33 -0500

    Routes usuario

--- server/server.js
+++ server/server.js
@@ -10,41 +10,14 @@ const bodyParser = require('body-parser')
 
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
+app.use( require('./routes/usuario'))
+
 
 app.get('/', function(req, res) {
     res.json('Hola mundo')
 })
 
-app.get('/usuario', function(req, res) {
-    res.json('Get Usuario')
-})
 
-app.post('/usuario', function(req, res) {
-    let body = req.body;
-
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        });
-    }else {
-        res.json({
-            body
-        })
-    }
-})
-
-
-app.put('/usuario/:id', function(req, res) {
-    let id = req.params.id;
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', function(req, res) {
-    res.json('Delete usuario')
-})
 
 mongoose.connect('mongodb://localhost:27017/cafe', (err, resp) => {
     if ( err ) throw err;
