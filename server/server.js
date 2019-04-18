385270e0154e227e6b0c47e220cc0ef55959774c
larrygf02 <raulgf_02@hotmail.com>
2019-04-18 16:21:25 -0500

    Conectandonos con mogoose

--- server/server.js
+++ server/server.js
@@ -1,5 +1,10 @@
 require('./config/config')
+
+
 const express = require('express')
+const mongoose = require('mongoose')
+
+
 const app = express()
 const bodyParser = require('body-parser')
 
@@ -41,6 +46,10 @@ app.delete('/usuario', function(req, res) {
     res.json('Delete usuario')
 })
 
+mongoose.connect('mongodb://localhost:27017/cafe', (err, resp) => {
+    if ( err ) throw err;
+    console.log('Base de datos conectada');
+});
 
 app.listen(process.env.PORT, () => {
     console.log('Escuchando el puerto 3000');
