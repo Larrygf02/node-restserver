7804db52fa57d0b1b638529fcbb3333a4ffe6a0c
larrygf02 <raulgf_02@hotmail.com>
2019-04-23 23:03:52 -0500

    PUT usuario

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -39,9 +39,22 @@ app.post('/usuario', function(req, res) {
 
 app.put('/usuario/:id', function(req, res) {
     let id = req.params.id;
-    res.json({
-        id
+    let body = req.body;
+    console.log(body);
+
+    Usuario.findOneAndUpdate(id, body, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+        res.json({
+            ok: true,
+            usuario: usuarioDB
+        })
     })
+
 })
 
 app.delete('/usuario', function(req, res) {
