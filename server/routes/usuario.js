89ce575360427b4031a8816385041c091a569bbc
larrygf02 <raulgf_02@hotmail.com>
2019-04-25 00:36:40 -0500

    Paginacion de usuarios

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -7,7 +7,28 @@ const _ = require('underscore');
 const Usuario = require('../models/usuario')
 
 app.get('/usuario', function(req, res) {
-    res.json('Get Usuario')
+    //res.json('Get Usuario')
+    let desde = req.query.desde || 0;
+    desde = Number(desde);
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite);
+    Usuario.find({})
+            .skip(desde)
+            .limit(limite)
+            .exec( (err, usuarios) => {
+                if (err) {
+                    return res.status(400).json({
+                        ok: false,
+                        err
+                    });
+                };
+
+                res.json({
+                    ok: true,
+                    usuarios
+                })
+            })
 })
 
 app.post('/usuario', function(req, res) {
