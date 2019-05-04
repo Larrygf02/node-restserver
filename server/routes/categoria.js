8b38ffc94b6a5f6584b4d1634b2b99434efbca21
larrygf02 <raulgf_02@hotmail.com>
2019-05-04 12:43:53 -0500

    Get and post categorias

---
+++ server/routes/categoria.js
@@ -0,0 +1,82 @@
+const express = require('express')
+
+let { verificaToken } = require('../middlewares/autenticacion');
+
+let app = express()
+
+let Categoria = require('../models/categoria')
+
+//Mostrar todas las categorias
+app.get('/categoria',verificaToken, (req,res) => {
+    Categoria.find( (err, categorias) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+        Categoria.count((err, conteo) => {
+            res.json({
+                ok: true,
+                categorias,
+                cuantos:conteo
+            })
+        })
+    })
+});
+
+//Mostrar una categoria por id
+app.get('/categoria/:id', verificaToken,(req,res) => {
+    let id = req.params.id;
+    Categoria.findById( id, (err, categoria) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+        res.json({
+            ok: true,
+            categoria
+        })
+    })
+});
+
+//Crear nueva categoria
+app.post('/categoria',verificaToken,(req,res) => {
+    let body = req.body;
+
+    let categoria = new Categoria({
+        descripcion: body.descripcion,
+        usuario: req.usuario._id
+    })
+    console.log(categoria);
+
+    categoria.save((err, categoriaDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        };
+
+        res.json({
+            ok: true,
+            categoria: categoriaDB
+        })
+    })
+});
+
+
+//Actualizar categoria
+app.put('/categoria/:id', (req,res) => {
+    
+});
+
+//Borrar categoria
+app.delete('/categoria/:id', (req,res) => {
+    //Solo un administrador puede borrar categorias
+});
+
+
+module.exports = app;
