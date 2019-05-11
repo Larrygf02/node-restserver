f6c889334bf9cd414257a8e0bc8c706123ae491b
larrygf02 <raulgf_02@hotmail.com>
2019-05-11 13:42:35 -0500

    Verifica token por url en la imagen

--- server/routes/imagenes.js
+++ server/routes/imagenes.js
@@ -3,17 +3,22 @@ const express = require('express')
 const fs = require('fs')
 const path = require('path')
 
+const {verificaTokenImg } = require('../middlewares/autenticacion')
 let app = express()
 
-app.get('/imagen/:tipo/:img', (req, res) => {
+app.get('/imagen/:tipo/:img', verificaTokenImg, (req, res) => {
     let tipo = req.params.tipo
     let img = req.params.img
 
-    let pathImg = `./uploads/${ tipo }/${ img }`;
-    let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg')
     //verificar si el path existe
-    //SendFile debe recibir un path absoluto
-    res.sendFile(noImagePath)
+    let pathImagen = path.resolve(__dirname, `../../uploads/${tipo}/${ img }`)
+    if ( fs.existsSync(pathImagen)){
+        res.sendFile(pathImagen)
+    } else {
+        let noImagePath = path.resolve(__dirname, '../assets/no-image.jpg')
+        //SendFile debe recibir un path absoluto
+        res.sendFile(noImagePath)
+    }
 })
 
 module.exports = app;
